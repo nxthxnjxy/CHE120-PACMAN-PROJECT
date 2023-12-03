887e4cdb8b00619cbbda4ed36fbb9f84e233fd41
nxthxnjxy <144192004+nxthxnjxy@users.noreply.github.com>
2023-12-03 13:39:31 -0500

    Update PACMANTIMER.py

--- PACMANTIMER.py
+++ PACMANTIMER.py
@@ -1,39 +1,58 @@
+"""Pacman, classic arcade game.
+
+Exercises
+
+1. Change the board. -DONE
+2. Change the number of ghosts. -DONE
+3. Change where pacman starts. -DONE
+4. Make the ghosts faster/slower. -DONE
+5. Make the ghosts smarter.
+"""
+
 from random import choice
 from turtle import *
-
 from freegames import floor, vector
+import time
 
-state = {'score': 0, 'timer': 120}
+
+time_limit = 120
+start_time = time.time()
+elapsed_time = int(time.time() - start_time)
+
+time_left = time_limit - elapsed_time
+
+state = {'score': 0}
 path = Turtle(visible=False)
 writer = Turtle(visible=False)
-aim = vector(5, 0)
+aim = vector(10, 0)
 pacman = vector(-80, 40)
 ghosts = [
     [vector(-180, 160), vector(10, 0)],
     [vector(-180, -160), vector(0, 10)],
     [vector(100, 160), vector(0, -10)],
     [vector(100, -160), vector(-10, 0)],
-    [vector(100, 150), vector(-10, 0)]
+    [vector(100, 150), vector(-10, 0)],
+    [vector(-150, 130), vector(10, 0)]
 ]
 # fmt: off
 tiles = [
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
-    0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0,
     0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
-    0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0,
+    0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0,
+    0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0,
+    0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0,
+    0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0,
+    0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0,
+    0, 1, 0, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0,
+    0, 1, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0,
+    0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0,
+    0, 1, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0,
     0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0,
-    0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0,
-    0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0,
-    0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
-    0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0,
-    0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0,
-    0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
-    0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0,
+    0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0,
     0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0,
-    0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0,
-    0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0,
-    0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0,
+    0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0,
+    0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0,
+    0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0,
     0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
@@ -100,7 +119,7 @@ def world():
 def move():
     """Move pacman and all ghosts."""
     writer.undo()
-    writer.write(f"Score: {state['score']}  Time: {state['timer']}s")
+    writer.write(state['score'])
 
     clear()
 
@@ -142,36 +161,9 @@ def move():
 
     for point, course in ghosts:
         if abs(pacman - point) < 20:
-            game_over()
             return
 
-    state['timer'] -= 1
-
-    if state['timer'] > 0:
-        ontimer(move, 100)
-    else:
-        game_over()
-
-
-def timer_update():
-    """Update timer display."""
-    writer.undo()
-    writer.write(f"Score: {state['score']}  Time: {state['timer']}s")
-    if state['timer'] > 0:
-        ontimer(timer_update, 1000)  # Update timer every 1000 milliseconds (1 second)
-
-
-def game_over():
-    """Display the game over screen."""
-    clear()
-    path.color('white')
-    path.goto(-50, 0)
-    path.write("Game Over", font=("Arial", 16, "normal"))
-    path.goto(-50, -20)
-    path.write(f"Your Score: {state['score']}", font=("Arial", 16, "normal"))
-    path.goto(-50, -40)
-    path.write("Thanks for playing!", font=("Arial", 16, "normal"))
-    update()
+    ontimer(move, 100)
 
 
 def change(x, y):
@@ -180,18 +172,21 @@ def change(x, y):
         aim.x = x
         aim.y = y
 
+if elapsed_time > time_limit:
+    writer.write("GAME OVER!")
+
 
 setup(420, 420, 370, 0)
 hideturtle()
 tracer(False)
 writer.goto(160, 160)
 writer.color('white')
+writer.write(state['score'])
+listen()
+onkey(lambda: change(10, 0), 'Right')
+onkey(lambda: change(-10, 0), 'Left')
+onkey(lambda: change(0, 10), 'Up')
+onkey(lambda: change(0, -10), 'Down')
 world()
 move()
-timer_update()
-listen()
-onkey(lambda: change(5, 0), 'Right')
-onkey(lambda: change(-5, 0), 'Left')
-onkey(lambda: change(0, 5), 'Up')
-onkey(lambda: change(0, -5), 'Down')
 done()
